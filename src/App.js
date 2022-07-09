16bc1279dc090fc56ed56356e45d249c92015d2b
Jeffrey <jdhill2142@gmail.com>
2022-07-09 16:24:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+import './fonts/akbar.ttf';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+// import simpsonsLogo from '../src/assets/yellow_logo.png';
+
+const App = () => {
+  const [quotes, setQuotes] = useState("");
+
+  
+  const getQuote = () => {
+    fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+    .then((res) => res.json())
+    .then((data) => {
+      let randomNum = Math.floor(Math.random() * data.length);
+      setQuotes(data[randomNum]);
+      console.log(data[randomNum]);
+      console.log(setQuotes);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/* <div><img src='../src/assets/yellow_logo.png' /></div> */}
+      
+      <h1 className='App-header'>The Simpsons...</h1>
+      <div className='quote-container'>
+       <div className='quote-text'>
+            {quotes.quote}
+        </div>
+        <div className='quote-author'>
+          {quotes.character}
+          <img className='img-simpson-character' src={quotes.image} alt='Simpsons character' />
+        </div>
+        <div className="button-container">
+          <button onClick={getQuote} className="twitter-button" id="twitter" title="Tweet this!">
+          <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+          </button>
+          <button onClick={getQuote} className='quote-button'>"New Quote" </button>
+        </div>
+      </div>
     </div>
   );
 }
